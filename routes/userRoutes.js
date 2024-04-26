27e1c5e052b5282c2e2a793f6b076ce7d72d083e
JacekGaw <jac.gawlyta@gmail.com>
2024-04-27 01:21:49 +0200

    Add endpoint to update user, improved error handling

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { getUsers, addUser, deleteUser, getUserById } from "../controllers/userController.js";
+import { getUsers, addUser, deleteUser, getUserById, updateUser } from "../controllers/userController.js";
 
 router.get("/getAll", getUsers);
 router.get("/getUserById", getUserById);
-router.post("/addUser", addUser);
+router.post("/register", addUser);
 router.delete("/delete", deleteUser);
+router.patch("/updateUser", updateUser);
+//TODO: Add endpoint for logging in, where using bcrypt password will be check
 
 export default router;
